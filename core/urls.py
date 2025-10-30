6ac74c37df7dc5225c2edfef378e3d38a5b72de7
sevapova <slatipova99@gmail.com>
2025-10-30 20:03:39 +0500

    urls

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view, about_view, index_view, greeting_view, get_products_view
+from pages.views import (
+    index_view, 
+    root_view, 
+    home_view, 
+    about_view, 
+    projects_view, 
+    contact_view, 
+    greeting_view, 
+    get_products_view)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', root_view, name='root'),
+    path('', index_view, name='index'),
     path('home/', home_view, name='home'),
     path('about/', about_view, name='about'),
-    path('', index_view, name='index'),
+    path('projects/', projects_view, name='projects'),
+    path('contact/', contact_view, name='contact'),
     path('greeting/<str:name>', greeting_view),
     path('products/<int:product_id>', get_products_view),
 ]
